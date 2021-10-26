1b49966169935df8360ff2ab3d086cb227abcf27
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-26 09:24:28 -0700

    move getmovies function

---
+++ src/components/getMovies.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default async function getMovies() {
+  const response = axios.get(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+  );
+
+  const movies = await response;
+//   const json = await response.json();
+  return movies.data.results
+};
--- src/components/movie_index.jsx
+++ src/components/movie_index.jsx
@@ -1,28 +1,16 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import styled from "styled-components";
 import MovieCard from "./movie_card";
+import getMovies from "./getMovies"
 
 
 const MovieIndex = () => {
   const [movieList, setMovieList] = useState([]);
 
-  const getMovies = () => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-      )
-      .then((res) => {
-        setMovieList(res.data.results);
-      });
-  };
-
   useEffect(() => {
-    async function fetchData() {
-      const response = await getMovies();
-      console.log(response);
-    }
-    return fetchData();
+      getMovies().then(res => {
+          setMovieList(res)
+      })
   }, []);
 
   const Header = styled.h1`
