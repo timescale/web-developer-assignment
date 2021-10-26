465390a2434c1823855bc1b7bc8a715445f24e67
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-25 17:23:36 -0700

    style modal

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import closeicon from "../images/close-icon.svg";
 
-const Modal = ({ visible, toggle, movieInfo }) =>
-  visible
+const Modal = ({ visible, toggle, movieInfo }) => {
+  const {
+    poster_path,
+    vote_average,
+    vote_count,
+    overview,
+    release_date,
+    title,
+  } = movieInfo;
+
+  return visible
     ? ReactDOM.createPortal(
         <div className="modal">
           <div className="modal-pop" role="dialog" aria-modal="true">
             {console.log(movieInfo)}
-            <h3>Hello World</h3>
-            <p>
-              Et sit saepe velit tenetur et consequatur in. Nihil doloribus
-              nulla nulla rem. Soluta illo et asperiores numquam earum nesciunt.
-              Vero odio voluptatem sunt sunt laboriosam.
-            </p>
-            <button type="button" onClick={toggle}>
-              Close
-            </button>
+            <div className="modal-header">
+              <h3>{title}</h3>
+              <img src={closeicon} alt="close-x" onClick={toggle} />
+            </div>
+            <div className="modal-content">
+              <img
+                className="modal-image"
+                src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                alt="movie-poster"
+              />
+              <div className="movie-info-container">
+                <p className="movie-info-header">
+                  <strong>Release date: </strong>
+                  {release_date}
+                </p>
+                <p className="movie-info-body">{overview}</p>
+                <p className="movie-rating">
+                  <strong>{vote_average}</strong>/10 ({vote_count} total votes)
+                </p>
+              </div>
+            </div>
           </div>
           <div className="modal-overlay"></div>
         </div>,
         document.body
       )
     : null;
+};
 
 export default Modal;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './modal.css';
 import App from './components/App';
 
 ReactDOM.render(
