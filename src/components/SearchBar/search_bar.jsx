37b57eb3e64de1d652fd253c289dac4c2c261d01
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-25 21:53:38 -0700

    add searchbar

--- src/components/SearchBar/search_bar.jsx
+++ src/components/SearchBar/search_bar.jsx
@@ -5,9 +5,10 @@ import search from '../../images/search.png'
 const SearchBar = () => {
     return (
         <form className="form">
-            <input type="text" placeholder={`${search} Search for a movie`}/>
-            <input type="submit" />
-            {/* <img  src={search} alt="search-icon" /> */}
+            <input className="search-field" type="search" placeholder="Search for a movie"/>
+            {/* <button className="search-button" type="submit"> */}
+                <img className="search-icon" src={search} alt="search-img" />
+            {/* </button> */}
         </form>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css';
 import './index.css';
 import './modal.css';
 import App from './components/App';
