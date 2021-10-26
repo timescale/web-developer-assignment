b4a8ce66ab33b40eafd280c039d90c9cdb60651e
Arianne Dimoff <ariannedimoff@gmail.com>
2021-10-25 21:57:00 -0700

    style searchbar

--- src/components/SearchBar/search_bar.jsx
+++ src/components/SearchBar/search_bar.jsx
@@ -5,10 +5,8 @@ import search from '../../images/search.png'
 const SearchBar = () => {
     return (
         <form className="form">
-            <input className="search-field" type="search" placeholder="Search for a movie"/>
-            {/* <button className="search-button" type="submit"> */}
+            <input className="search-field" type="text" placeholder="Search for a movie"/>
                 <img className="search-icon" src={search} alt="search-img" />
-            {/* </button> */}
         </form>
     )
 };
