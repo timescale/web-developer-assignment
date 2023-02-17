e6e216e6b0f59919545d5bb98ba7fdd215e2c012
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-17 19:14:29 -0300

    Add theme provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Theme from "./styles"
+
+import { ThemeProvider } from "styled-components"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
---
+++ src/styles.js
@@ -0,0 +1,29 @@
+const Theme = {
+  color: {
+    background: "#FFFFFF",
+    primary: {
+      main: "#FDB515",
+      contrastText: "#141E35",
+    },
+    secondary: {
+      main: "#1472EC",
+      contrastText: "#FFFFFF",
+    },
+    error: "#F54545",
+    border: "#898E9A",
+  },
+  font: {
+    size: {
+      caption: "0.75",
+      label: "0.875rem",
+      title: "2rem",
+      subtitle: "1.5rem",
+    },
+    family: {
+      sans: "Inter",
+      mono: "JetBrains Mono",
+    },
+  },
+}
+
+export default Theme
